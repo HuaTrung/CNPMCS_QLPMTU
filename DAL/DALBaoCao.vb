6bb043d4f49e3d489108a583dc90a91aac4e4f85
Gnurt <huavitrung16@gmail.com>
2018-05-15 00:26:10 +0700

    DanhSachBenhNhan+BaoCao(1/3)

--- DAL/DALBaoCao.vb
+++ DAL/DALBaoCao.vb
@@ -8,13 +8,34 @@ Public Class DALBaoCao
     Shared Command As New MySqlCommand
     Public Shared Function DAL_BaoCaoDoanhThu(thang As String, nam As String) As DataTable
         Dim dt As New DataTable
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayBaoCaoDoanhThuTheoNgay('" & thang & "','" & nam & "')", conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
 
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 
     Public Shared Function DAL_BaoCaoSuDungThuoc(thang As String, nam As String) As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayBaoCaoSuDungThuoc('" & thang & "','" & nam & "')", conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 End Class
--- DAL/DALDanhSachBenhNhan.vb
+++ DAL/DALDanhSachBenhNhan.vb
@@ -10,17 +10,54 @@ Public Class DALDanhSachBenhNhan
 
     Public Shared Function DAL_LayDanhSachBenhNhan() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayDanhSachBenhNhan()", conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
     Public Shared Function DAL_timkiembenhnhantrongdanhsach(temphoten As String, temploaibenh As String, strtungay As String, strdenngay As String) As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        If String.IsNullOrEmpty(strtungay) Then
+            strtungay = "2017-04-01"
+        End If
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call timkiembenhnhantrongdanhsach('" & temphoten & "','" & temploaibenh & "','" & strtungay & "','" & strdenngay & "')"
+            Command = New MySqlCommand(Query, conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
     Public Shared Function DAL_LayThongTinBenhNhands(temphoten As String, tempngaykham As String) As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call LayThongTinBenhNhands('" & temphoten & "','" & tempngaykham & "')"
+            Command = New MySqlCommand(Query, conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 End Class
