54893dfe987bd43aac06f76afe9fb32d34605218
Gnurt <huavitrung16@gmail.com>
2018-05-18 00:01:36 +0700

    finished Baocao under SQL + some parts of KhamBenh
    Hoàn thành mục báo cáo ở các câu truy vấn dưới database. Thêm 1 vài chức năng cho phần khám bệnh.

--- DAL/DALKhamBenh.vb
+++ DAL/DALKhamBenh.vb
@@ -9,7 +9,28 @@ Public Class DALKhamBenh
     Shared Command As New MySqlCommand
     Public Shared Function DAL_LayDuLieuBnMBN(mbn As String) As BenhNhan
         Dim bncanlay As New BenhNhan
-
+        ' Dim SDA As New MySqlDataAdapter
+        Dim myReader As MySqlDataReader
+        Try
+            conn.Open()
+            Command = New MySqlCommand("CALL LayDuLieuBenhNhanKhiBietMaBenhNhan('" & mbn & "')", conn)
+            myReader = Command.ExecuteReader()
+            While myReader.Read()
+                bncanlay.mabenhnhan = myReader("MaBenhNhan").ToString()
+                bncanlay.hoten = myReader("Hoten").ToString()
+                bncanlay.CMND = myReader("cmnd").ToString()
+                bncanlay.gioitinh = myReader("Gioitinh").ToString()
+                bncanlay.diachi = myReader("DiaChi").ToString()
+                bncanlay.didong = myReader("DiDong").ToString()
+                bncanlay.namsinh = myReader("NamSinh").ToString()
+                bncanlay.nghenghiep = myReader("NgheNghiep").ToString()
+            End While
+            myReader.Close()
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return bncanlay
     End Function
 
@@ -17,12 +38,32 @@ Public Class DALKhamBenh
     'Init necessary variables to connect to the database
     Public Shared Function DAL_LoadDataDaKhamFromDatabase() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayDuLieuBenhNhanDaKhamTrongNgay(now())", conn)
+            SDA.SelectCommand = Command
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
     Public Shared Function DAL_LoadDataChuaKhamFromDatabase() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayDuLieuBenhNhanChuaKhamTrongNgay(now())", conn)
+            SDA.SelectCommand = Command
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 
