cfaa4e8956b4a6f49d7807f2dafa28574a9b02c6
Gnurt <huavitrung16@gmail.com>
2018-05-22 01:26:31 +0700

    Full kham benh ( hope so)

--- DAL/DALKhamBenh.vb
+++ DAL/DALKhamBenh.vb
@@ -69,33 +69,123 @@ Public Class DALKhamBenh
 
     Public Shared Function DAL_TaiDanhSachThuoc() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayDanhSachThuoc()", conn)
+            SDA.SelectCommand = Command
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 
 #End Region
     Public Shared Function DAL_TimKiemThuoc(tenthuoc As String) As DataTable
         Dim dt As New DataTable
-
+        Dim coNhieuHon1DieuKien As Boolean = False
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Dim Query As String
+            If (tenthuoc.Length = 0) Then
+                Query = "call LayDuLieuThuoc()"
+            Else
+                Query = "call TimKiemThuoc('" & tenthuoc & "')"
+            End If
+            Command = New MySqlCommand(Query, conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
     Public Shared Sub DAL_khoitaophieukhambenh(mbn As String)
-
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call KhoiTaoPhieuKhamBenh(now(),'" & mbn & "')"
+            Command = New MySqlCommand(Query, conn)
+            READER = Command.ExecuteReader
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
     End Sub
     Public Shared Sub DAL_LuuChiTietPhieuKhamBenh(thuoc As Thuoc, mpkb As String)
-
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call ThemChiTietPhieuKhamBenh('" & mpkb & "','" & thuoc.TenThuoc & "','" & thuoc.DonVi & "','" & thuoc.CachDung & "','" & thuoc.SoLuong & "')"
+            Command = New MySqlCommand(Query, conn)
+            READER = Command.ExecuteReader
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
     End Sub
     Public Shared Sub DAL_LuuTrieuChungLoaiBenhVaoPhieuKhamBenh(TrieuChung As String, chandoanbenh As String, maphieukhambenh As String)
-
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call ThemTrieuChungVaLoaiBenhVaoPhieuKhamBenh('" & TrieuChung & "','" & chandoanbenh & "','" & maphieukhambenh & "')"
+            Command = New MySqlCommand(Query, conn)
+            READER = Command.ExecuteReader
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
     End Sub
     Public Shared Function DAL_LayMaPhieuKhamBenh(mbn As String) As String
         Dim _maphieukhambenh As String = ""
-
+        ' Dim SDA As New MySqlDataAdapter
+        Dim myReader As MySqlDataReader
+        Try
+            conn.Open()
+            Command = New MySqlCommand("CALL LayMaPhieuKhamBenh('" & mbn & "',now())", conn)
+            myReader = Command.ExecuteReader()
+            While myReader.Read()
+                _maphieukhambenh = myReader("MaPhieuKhamBenh").ToString()
+            End While
+            'SDA.SelectCommand = Command
+            ' SDA.Fill(dt)
+            myReader.Close()
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return _maphieukhambenh
     End Function
     Public Shared Function DAL_taidulieuvaoloaibenh() As List(Of String)
         Dim list As New List(Of String)()
-
+        ' Dim SDA As New MySqlDataAdapter
+        Dim myReader As MySqlDataReader
+        Try
+            conn.Open()
+            Command = New MySqlCommand("CALL LayDanhSachBenh()", conn)
+            myReader = Command.ExecuteReader()
+            While myReader.Read()
+                list.Add(myReader.GetString("LoaiBenh"))
+            End While
+            myReader.Close()
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return list
     End Function
 #End Region
