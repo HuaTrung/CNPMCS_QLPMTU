ad12719255cd8ec43484a7bf3ca99f20f24ede24
Gnurt <huavitrung16@gmail.com>
2018-05-29 00:42:43 +0700

    full thuoc + thay doi qui dinh

--- DAL/DALThayDoiQuiDinh.vb
+++ DAL/DALThayDoiQuiDinh.vb
@@ -7,12 +7,34 @@ Public Class DALThayDoiQuiDinh
     Shared conn As New MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings("ketnoicosodulieu").ConnectionString)
     Shared Command As New MySqlCommand
     Public Shared Function DAL_chinhsuaquidinh(_soluongbenhnhankham As String, _soluongloaibenh As String, _soluongthuoc As String, _soluongdonvi As String, _soluongcachdung As String) As Integer
-
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "call ThayDoiQuiDinh('" & _soluongbenhnhankham & "','" & _soluongloaibenh & "','" & _soluongthuoc & "','" & _soluongdonvi & "','" & _soluongcachdung & "')"
+            Command = New MySqlCommand(Query, conn)
+            READER = Command.ExecuteReader
+            conn.Close()
+        Catch ex As MySqlException
+            Return -1
+        Finally
+            conn.Dispose()
+        End Try
         Return 1
     End Function
     Public Shared Function DAL_laythamso() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("select SoBenhNhanToiDa,SoLuongLoaiBenh,SoLuongThuoc,SoLuongDonViTinh,SoLuongCachDung from thamso limit 1", conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 End Class
--- DAL/DALThuoc.vb
+++ DAL/DALThuoc.vb
@@ -8,12 +8,39 @@ Public Class DALThuoc
     Shared Command As New MySqlCommand
     Public Shared Function DAL_TaiDuLieuThuoc() As DataTable
         Dim dt As New DataTable
-
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Command = New MySqlCommand("call LayDuLieuThuoc()", conn)
+            SDA.SelectCommand = Command
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
     Public Shared Function DAL_TimKiemThuoctrongDanhSachThuoc(text As String) As DataTable
         Dim dt As New DataTable
-
+        Dim coNhieuHon1DieuKien As Boolean = False
+        Dim SDA As New MySqlDataAdapter
+        Try
+            conn.Open()
+            Dim Query As String
+            If (text.Length = 0) Then
+                Query = "call LayDuLieuThuoc()"
+            Else
+                Query = "call TimKiemThuoctrongDanhSachThuoc('" & text & "')"
+            End If
+            Command = New MySqlCommand(Query, conn)
+            SDA = New MySqlDataAdapter(Command)
+            SDA.Fill(dt)
+            conn.Close()
+        Catch ex As MySqlException
+        Finally
+            conn.Dispose()
+        End Try
         Return dt
     End Function
 
